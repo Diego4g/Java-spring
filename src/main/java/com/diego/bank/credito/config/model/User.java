95243275aaf7cc9f829b36d6f9d6b6096c0d4723
Diego Ferreira <diegodejesusferreira2@gmail.com>
2022-11-22 21:48:07 -0300

    feat: preparing mvc

---
+++ src/main/java/com/diego/bank/credito/config/model/User.java
@@ -0,0 +1,12 @@
+package com.diego.bank.credito.config.model;
+
+import javax.persistence.Table;
+
+@Table(name="tb_user", schema = "message_generator")
+public class User {
+
+    private String name;
+    private String lastName;
+    private String login;
+    private Integer age;
+}
